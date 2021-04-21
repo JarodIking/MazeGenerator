898dda3f5dbe6f71285f74ba368d9b0294190be4
jarod <jarodiking17@gmail.com>
2021-04-21 21:21:16 +0200

    version 0.1.4
    fixed bug thats caused the stack to break, added namespace also fixed screenwidth by adding paratheses

--- mazeGenerater/mazeGenerater/mazeGenerater.cpp
+++ mazeGenerater/mazeGenerater/mazeGenerater.cpp
@@ -1,48 +1,54 @@
 // mazeGenerater.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+#pragma once
 
 #include <iostream>
 #include <stack>
+using namespace std;
+
+
 #include "olcConsoleEngine.h"
 
+
 class mazeGenerator : public olcConsoleGameEngine {
-    public mazeGenerator() {
-        m_s AppName = L"MAZE";
+    public: 
+    
+    mazeGenerator(){
+        m_sAppName = L"MAZE";
     }
 
 private:
-    int mazeWidth;
-    int maxeHeight;
-    int maze;
+    int m_nMazeWidth;
+    int m_nMazeHeight;
+    int *m_maze;
 
-    enum {
-        CELL_PATH_N = 0x01;
-        CELL_PATH_E = 0x02;
-        CELL_PATH_S = 0x04;
-        CELL_PATH_W = 0x08;
-        CELL_VISITED = 0x10;
-
+    enum{
+        CELL_PATH_N = 0x01,
+        CELL_PATH_E = 0x02,
+        CELL_PATH_S = 0x04,
+        CELL_PATH_W = 0x08,
+        CELL_VISITED = 0x10,
     };
 
-    int visitedCells;
+    int m_nVisitedCells;
 
-    stack<pair<int, int>> stack;
+    stack<pair<int , int>> m_stack;	// (x, y) coordinate pairs
 
 
 protected:
     virtual bool OnUserCreate() {
         //initialize alogrithm
-        mazeWidth = 40;
-        mazeHeight = 25;
+        m_nMazeWidth = 40;
+        m_nMazeHeight = 25;
 
         //allocate maze memory
-        maze = new int[mazeWidth * mazeHeight];
-        memset(m_maze, 0x00, mazeWidth * mazeHeight);
+        m_maze = new int[m_nMazeWidth * m_nMazeHeight];
+        memset(m_maze, 0x00, m_nMazeWidth * m_nMazeHeight);
         
         //push cells to stack
-        stack.push(make_pair(0, 0));
-        maze[0] = CELL_VISITED;
-        visitedCells = 1;
+        m_stack.push(make_pair(0, 0));
+        m_maze[0] = CELL_VISITED;
+        m_nVisitedCells = 1;
 
         return true;
     }
@@ -51,11 +57,11 @@ protected:
 
         // draw stuff
 
-        Fill(0, 0, screenWidth, screenHeight, "L");
+        Fill(0, 0, ScreenWidth(), ScreenHeight(), L' ');
 
-            for (int x = 0; x < mazeWidth; x++) {
-                for (int y = 0; y < mazeHeight; y++) {
-                    if (maze[y * mazeWidth + x] & CELL_VISITED) {
+            for (int x = 0; x < m_nMazeWidth; x++) {
+                for (int y = 0; y < m_nMazeHeight; y++) {
+                    if (m_maze[y * m_nMazeWidth + x] & CELL_VISITED) {
                         Draw(x, y, PIXEL_SOLID, FG_WHITE);
                     }
                     else {
@@ -73,7 +79,9 @@ int main()
     //create maze and run
     mazeGenerator game;
     game.ConstructConsole(160, 100, 8, 8);
-    game.start();
+    game.Start();
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
