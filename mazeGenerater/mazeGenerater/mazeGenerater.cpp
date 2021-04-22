7040ffd3d61859ed3b12d128627e1eb5cc66991f
jarod <jarodiking17@gmail.com>
2021-04-22 19:19:15 +0200

    version 1.0.0
    it's almost done, added in the algorthim and backtracking.

--- mazeGenerater/mazeGenerater/mazeGenerater.cpp
+++ mazeGenerater/mazeGenerater/mazeGenerater.cpp
@@ -1,14 +1,14 @@
 // mazeGenerater.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-#pragma once
 
 #include <iostream>
 #include <stack>
+#include <vector>
+#include <conio.h>
+#include <windows.h>
 using namespace std;
 
 
 #include "olcConsoleEngine.h"
-
 
 class mazeGenerator : public olcConsoleGameEngine {
     public: 
@@ -34,26 +34,105 @@ private:
 
     stack<pair<int , int>> m_stack;	// (x, y) coordinate pairs
 
+    int m_nPathWidth;
+
 
 protected:
-    virtual bool OnUserCreate() {
-        //initialize alogrithm
-        m_nMazeWidth = 40;
-        m_nMazeHeight = 25;
-
-        //allocate maze memory
+    bool OnUserCreate() {
+        // Maze parameters
+        m_nMazeWidth = 30;
+        m_nMazeHeight = 30;
         m_maze = new int[m_nMazeWidth * m_nMazeHeight];
-        memset(m_maze, 0x00, m_nMazeWidth * m_nMazeHeight);
-        
-        //push cells to stack
-        m_stack.push(make_pair(0, 0));
-        m_maze[0] = CELL_VISITED;
+        memset(m_maze, 0x00, m_nMazeWidth * m_nMazeHeight * sizeof(int));
+        m_nPathWidth = 3;
+
+        // Choose a starting cell
+        int x = rand() % m_nMazeWidth;
+        int y = rand() % m_nMazeHeight;
+        m_stack.push(make_pair(x, y));
+        m_maze[y * m_nMazeWidth + x] = CELL_VISITED;
         m_nVisitedCells = 1;
 
         return true;
     }
 
      bool OnUserUpdate(float fElppasedTime) override {
+         //do maze algorithm
+         if (m_nVisitedCells < m_nMazeWidth * m_nMazeHeight) {
+             //step 1: Create a set of the unvisited neighbours
+
+             auto offset = [&](int x, int y) {
+                 return (m_stack.top().second + y) * m_nMazeWidth + (m_stack.top().first + x);
+             };
+
+
+             vector<int> neighbours;
+             //north neighbour
+             if (m_stack.top().second > 0 && (m_maze[offset(0, -1)] & CELL_VISITED) == 0)
+                 neighbours.push_back(0);
+                   
+             //east neighbour
+             if (m_stack.top().first < m_nMazeWidth - 1 && (m_maze[offset(1, 0)] & CELL_VISITED) == 0) 
+                 neighbours.push_back(1);
+             
+             //south neighbour
+             if (m_stack.top().second < m_nMazeWidth -1 && (m_maze[offset(0, 1)] & CELL_VISITED) == 0) 
+                 neighbours.push_back(2);
+             
+             //south neighbour
+             if (m_stack.top().first > 0 && (m_maze[offset(-1, 0)] & CELL_VISITED) == 0) 
+                 neighbours.push_back(3);
+             
+
+             //are there any neighbours available?
+             if (!neighbours.empty()) {
+                 //choose one neighbour at random 
+                 int next_cell_dir = neighbours[rand() % neighbours.size()];
+
+                 //create a path between the neighbour and the current cell
+                 switch (next_cell_dir)
+                 {
+                 case 0: //north
+                     m_maze[offset(0, -1)] |= CELL_VISITED | CELL_PATH_S;
+                     m_maze[offset(0, 0)] |= CELL_PATH_N;
+                     m_stack.push(make_pair((m_stack.top().first + 0), (m_stack.top().second - 1)));
+                     break;
+
+                 case 1: //east
+                     m_maze[offset(+1, 0)] |= CELL_VISITED | CELL_PATH_W;
+                     m_maze[offset(0, 0)] |= CELL_PATH_E;
+                     m_stack.push(make_pair((m_stack.top().first + 1), (m_stack.top().second + 0)));
+                     break;
+
+                 case 2: //south
+                     m_maze[offset(0, +1)] |= CELL_VISITED | CELL_PATH_N;
+                     m_maze[offset(0, 0)] |= CELL_PATH_S;
+                     m_stack.push(make_pair((m_stack.top().first + 0), (m_stack.top().second + 1)));
+                     break;
+
+                 case 3: //west
+                     m_maze[offset(-1, 0)] |= CELL_VISITED | CELL_PATH_E;
+                     m_maze[offset(0, 0)] |= CELL_PATH_E;
+                     m_stack.push(make_pair((m_stack.top().first - 1), (m_stack.top().second + 0)));
+                     break;
+
+                     //default:
+                         //break;
+                 }
+
+                 //new cell
+                 m_maze[offset(0, 0)] |= CELL_VISITED;
+                 m_nVisitedCells++;
+             }
+             else {
+                 m_stack.pop(); //Backtrack
+             }
+
+             
+
+         }
+         
+
 
         // draw stuff
 
@@ -61,11 +140,26 @@ protected:
 
             for (int x = 0; x < m_nMazeWidth; x++) {
                 for (int y = 0; y < m_nMazeHeight; y++) {
-                    if (m_maze[y * m_nMazeWidth + x] & CELL_VISITED) {
-                        Draw(x, y, PIXEL_SOLID, FG_WHITE);
-                    }
-                    else {
-                        Draw(x, y, PIXEL_SOLID, FG_BLUE);
+                    for (int py = 0; py < m_nPathWidth; py++) {
+                        for (int px = 0; px < m_nPathWidth; px++) {
+                            if (m_maze[y * m_nMazeWidth + x] & CELL_VISITED) {
+                                Draw(x * (m_nPathWidth + 1) + px, y * (m_nPathWidth + 1) + py, PIXEL_SOLID, FG_WHITE);
+                            }
+                            else {
+                                Draw(x * (m_nPathWidth + 1) + px, y * (m_nPathWidth + 1) + py, PIXEL_SOLID, FG_BLUE);
+                            }
+                        }
+
+                        for (int p = 0; p < m_nPathWidth; p++) {
+                            if (m_maze[y * m_nMazeWidth + x] & CELL_PATH_S)
+                                Draw(x * (m_nPathWidth + 1) + p, y * (m_nPathWidth + 1) + m_nPathWidth);
+
+
+                            if (m_maze[y * m_nMazeWidth + x] & CELL_PATH_E)
+                                Draw(x * (m_nPathWidth + 1) + m_nPathWidth, y * (m_nPathWidth + 1) + p);
+
+
+                        }
                     }
                 }
             }
@@ -76,6 +170,7 @@ protected:
 
 int main()
 {
+    system("Color 1A");
     //create maze and run
     mazeGenerator game;
     game.ConstructConsole(160, 100, 8, 8);
